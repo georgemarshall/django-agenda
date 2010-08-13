c9551f8cf634c464df53ac151a403704e59023e2
Guillaume Libersat <guillaume@spreadband.com>
2010-08-13 11:43:15 +0200

    remove location fields from admin

--- agenda/admin.py
+++ agenda/admin.py
@@ -6,17 +6,17 @@ from django.utils.translation import ugettext as _
 from .models import Event, Calendar
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'event_date', 'start_time', 'location', 'publish')
+    list_display = ('title', 'author', 'event_date', 'start_time', 'publish')
     list_display_links = ('title', )
-    list_filter = ('event_date', 'publish', 'author', 'location')
+    list_filter = ('event_date', 'publish', 'author')
 
     date_hierarchy = 'event_date'
     
     prepopulated_fields = {"slug": ("title",)}
     
-    search_fields = ('title', 'location__title', 'author__username', 'author__first_name', 'author__last_name', 'calendars')
+    search_fields = ('title', 'author__username', 'author__first_name', 'author__last_name', 'calendars')
 
-    fieldsets =  ((None, {'fields': ['title', 'slug', 'event_date', 'start_time', 'end_time', 'location', 'description']}),
+    fieldsets =  ((None, {'fields': ['title', 'slug', 'event_date', 'start_time', 'end_time', 'description']}),
                   (_('Advanced options'), {'classes' : ('collapse',),
                                            'fields'  : ('publish_date', 'publish', 'sites', 'author', 'allow_comments')}))
     
