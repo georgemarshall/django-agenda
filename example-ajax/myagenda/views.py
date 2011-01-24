c28766b1c177dcde96bc637468e56c92784b274c
Alban Tiberghien <alban@epplug-laptop.(none)>
2011-01-25 00:24:43 +0100

    * Prepare form processing * Deal with form errors

--- example-ajax/myagenda/views.py
+++ example-ajax/myagenda/views.py
@@ -4,12 +4,23 @@ from agenda.models import Recurrence, Event
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from .forms import EventForm, RecurrenceForm
+from django.http import HttpResponse
 
 
 def create_event(request):
-    event_form = EventForm()
-    recurrence_form = RecurrenceForm()
-    
+    if request.method == "POST":
+        event_form = EventForm(request.POST)
+        recurrence_form = RecurrenceForm(request.POST)
+        if event_form.is_valid():
+            event_form.save()
+            #TODO: replace HttpResponse by a good HttpResponseRedirect
+            return HttpResponse()
+        else:
+            print event_form.errors
+    else:
+        event_form = EventForm()
+        recurrence_form = RecurrenceForm()
+        
     return render_to_response("event_form.html",
                               {'event_form':event_form,
                                'recurrence_form':recurrence_form},
