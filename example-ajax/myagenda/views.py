66d1e2eca990c8a1b362e934eb7331d73dd3cf72
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-01-27 23:51:44 +0100

    WIP : process recurrence in view TODO : fix pb with selectbox keys for frequencies

--- example-ajax/myagenda/views.py
+++ example-ajax/myagenda/views.py
@@ -8,13 +8,22 @@ from django.http import HttpResponse
 
 
 def create_event(request):
+    has_recurrence = False
     if request.method == "POST":
         event_form = EventForm(request.POST)
-        recurrence_form = RecurrenceForm(request.POST)
         if event_form.is_valid():
-            event_form.save()
+            event = event_form.save(commit=False)
+            has_recurrence = request.POST.get('recurrence', None) 
+            if has_recurrence:
+                has_recurrence = True #instead of "on"
+                recurrence_form = RecurrenceForm(request.POST, initial={'base_event' : event})
+                if recurrence_form.is_valid():
+                    event.save()
+                    recurrence_form.save()
+                    return HttpResponse() 
             #TODO: replace HttpResponse by a good HttpResponseRedirect
-            return HttpResponse()
+            else:
+                return HttpResponse()
         else:
             print event_form.errors
     else:
@@ -23,5 +32,6 @@ def create_event(request):
         
     return render_to_response("event_form.html",
                               {'event_form':event_form,
-                               'recurrence_form':recurrence_form},
+                               'recurrence_form':recurrence_form,
+                               'has_recurrence' : has_recurrence},
                                context_instance=RequestContext(request))
