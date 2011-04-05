0dc66a8fe04820529837883ba50059a2eb03535e
George Marshall <echosx@gmail.com>
2011-04-05 01:57:55 -0700

    Added end date to admin interface

--- agenda/admin.py
+++ agenda/admin.py
@@ -11,23 +11,28 @@ class EventAdmin(admin.ModelAdmin):
     list_filter = ('begin_date', 'publish', 'author')
 
     date_hierarchy = 'begin_date'
-    
-    prepopulated_fields = {"slug": ("title",)}
-    
+
+    prepopulated_fields = {'slug': ('title',)}
+
     search_fields = ('title', 'author__username', 'author__first_name', 'author__last_name', 'calendars')
 
-    fieldsets =  ((None, {'fields': ['title', 'slug', 'begin_date', 'start_time', 'end_time', 'description']}),
-                  (_('Advanced options'), {'classes' : ('collapse',),
-                                           'fields'  : ('publish_date', 'publish', 'sites', 'author', 'allow_comments')}))
-    
+    fieldsets = (
+        (None, {
+            'fields': ['title', 'slug', 'begin_date', 'start_time', 'end_date', 'end_time', 'description']
+        }),
+        (_('Advanced options'), {
+            'classes' : ('collapse',),
+            'fields': ('publish_date', 'publish', 'sites', 'author', 'allow_comments')
+        })
+    )
+
     # This is a dirty hack, this belongs inside of the model but defaults don't work on M2M
     def formfield_for_dbfield(self, db_field, **kwargs):
         """ Makes sure that by default all sites are selected. """
         if db_field.name == 'sites': # Check if it's the one you want
             kwargs.update({'initial': Site.objects.all()})
-         
-        return super(EventAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-    
-admin.site.register(Event, EventAdmin)
 
+        return super(EventAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Calendar)
