2ddedd51a850ba80684a5c2c45bc29da125f3e0b
George Marshall <echosx@gmail.com>
2011-04-05 01:41:13 -0700

    Whitespace cleanup

--- agenda/models.py
+++ agenda/models.py
@@ -60,7 +60,7 @@ class Event(AbstractEvent):
         ('V', _('Valid')),
         ('C', _('Canceled')),
         ('P', _('Postponed')),
-        )
+    )
 
     class Meta:
         ordering = ['-begin_date', '-start_time', '-title']
@@ -69,17 +69,21 @@ class Event(AbstractEvent):
         unique_together = ("begin_date", "slug")
 
     def __unicode__(self):
-        return _("%(title)s from %(start_datetime)s to %(end_datetime)s") % { 'title'      : self.title,
-                                                                             'start_datetime' : self.start_datetime,
-                                                                             'end_datetime' : self.end_datetime }
-    @models.permalink                                               
+        return _("%(title)s from %(start_datetime)s to %(end_datetime)s") % {
+            'title': self.title,
+            'start_datetime': self.start_datetime,
+            'end_datetime': self.end_datetime
+        }
+
+    @models.permalink
     def get_absolute_url(self):
-        return ('agenda-detail', (), {'year'  : self.begin_date.year, 
-                                      'month' : self.begin_date.month, 
-                                      'day'   : self.begin_date.day, 
-                                      'slug'  : self.slug }
-                )
-        
+        return ('agenda-detail', (), {
+            'year': self.begin_date.year,
+            'month': self.begin_date.month,
+            'day': self.begin_date.day,
+            'slug': self.slug
+        })
+
     # Core fields
     state = models.CharField(_('state'), max_length=1, default='V', choices=STATE_CHOICES)
     title = models.CharField(_('title'), max_length=255)
@@ -90,51 +94,51 @@ class Event(AbstractEvent):
                          editable=True)
 
     #Relative to begin_date
-    start_time = models.TimeField(_('start time'), default=time(12), blank=True, null=True)
+    start_time = models.TimeField(_('start time'), blank=True, null=True)
     #Relative to end_date
-    end_time = models.TimeField(_('end time'), default=time(12), blank=True, null=True)
-    
+    end_time = models.TimeField(_('end time'), blank=True, null=True)
+
     description = models.TextField(_('description'), blank=True, null=True)
 
     # Extra fields
     add_date = models.DateTimeField(_('add date'), auto_now_add=True)
     mod_date = models.DateTimeField(_('modification date'), auto_now=True)
-    
+
     author = models.ForeignKey(User, verbose_name=_('author'), db_index=True, blank=True, null=True)
-    
+
     allow_comments = models.BooleanField(_('Allow comments'), default=True)
 
     @property
     def start_datetime(self):
         return datetime.combine(self.begin_date, self.start_time)
-    
+
     @property
     def end_datetime(self):
         return datetime.combine(self.end_date, self.end_time)
-    
+
     @property
     def duration(self):
         return self.end_datetime - self.start_datetime
-    
+
     @property
     def is_base_event(self):
         if hasattr(self, "recurrence"):
             return self.recurrence.count() > 0
         return False
-    
+
     @property
     def is_recurrence(self):
         if hasattr(self, "parent_recurrence"):
             return self.parent_recurrence.count() > 0
         return False
-    
+
 # ping_google can be called by a signal
 # TODO rewrite ping_google to be callable by a signal (e.g. add kwargs param)
 #post_save.connect(Event, ping_google)
 
 class MetaEvent(Event):
     sub_events = models.ManyToManyField(Event, related_name='meta_event', blank=True, null=True)
-    
+
     class Meta:
         verbose_name = _('meta event')
         verbose_name_plural = _('meta events')
@@ -162,23 +166,22 @@ class Recurrence(models.Model):
     """
     base_event = models.ForeignKey(Event, verbose_name=_('base event'), related_name='recurrence')
     recurrent_events = models.ManyToManyField(Event, verbose_name=_('recurrent events'), related_name='parent_recurrence')
-    
+
     frequency = models.SmallIntegerField(_('frequency'), choices=FREQUENCY_CHOICES)
-    
+
     # in most cases, should be base_event.start_datetime
     start_datetime = models.DateTimeField(_('recurrence begin date'), blank=True, null=True) #rrule dtstart
     end_datetime = models.DateTimeField(_('end date'), blank=True, null=True) # rrule until
-    
+
     interval = models.PositiveIntegerField(_('interval'), blank=True, null=True)
     count = models.PositiveIntegerField(_('count'), blank=True, null=True)
-    
+
     def save(self, **kwargs):
         if not self.start_datetime:
             self.start_datetime = self.base_event.start_datetime
-        
-        self.start_datetime = self.start_datetime.replace(minute=self.base_event.start_time.minute,
-                                                          hour=self.base_event.start_time.hour)
-        
+
+        self.start_datetime = self.start_datetime.replace(minute=self.base_event.start_time.minute, hour=self.base_event.start_time.hour)
+
         if not self.count and not self.end_datetime:
             self.end_datetime = self.start_datetime + relativedelta(years=+1)
 
@@ -198,14 +201,7 @@ def create_recurrence(sender, instance, created, **kwargs):
         for o in occurs:
             if o != instance.start_datetime:
                 base_event = instance.base_event
-                event = event_class.objects.create(title=base_event.title,
-                                                   begin_date = o,
-                                                   start_time = base_event.start_time,
-                                                   end_date = o + base_event.duration,
-                                                   end_time = base_event.end_time,
-                                                   description = base_event.description)
+                event = event_class.objects.create(title=base_event.title, begin_date=o, start_time=base_event.start_time, end_date=o + base_event.duration, end_time=base_event.end_time, description=base_event.description)
                 instance.recurrent_events.add(event)
     else:
         pass
-    
-    
